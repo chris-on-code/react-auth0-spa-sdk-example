1601ec2f699ec9fe423baf8fac3e81a0d71d2a1f
Chris on Code <chris@scotch.io>
2019-09-26 14:02:44 -0700

    adding more on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useAuth0 } from '../contexts/auth0-context';
 
 export default function Home() {
+  const { isLoading, user } = useAuth0();
+
   return (
     <div className="hero is-info is-fullheight">
       <div className="hero-body">
         <h1>Home Page</h1>
+
+        {/* show user info if they are logged in */}
+        {!isLoading && user && (
+          <>
+            <p>Hello {user.name}</p>
+            {user.picture && <img src={user.picture} alt="My Avatar" />}
+          </>
+        )}
       </div>
     </div>
   );

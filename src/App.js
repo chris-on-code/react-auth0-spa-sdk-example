23f4d5ef4cfeee9c7db2f635acbff2ba5611778a
Chris Sevilleja <chris@scotch.io>
2019-10-08 13:15:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
+import './App.css';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
